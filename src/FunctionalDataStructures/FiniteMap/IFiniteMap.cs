3403347edeaa43e6a2ef90ef81d7baac3354ba37
Jan Schwinghammer <j.schwinghammer@gmail.com>
2017-05-31 22:35:40 +0200

    interface renamed

--- src/FunctionalDataStructures/FiniteMap/IFiniteMap.cs
+++ src/FunctionalDataStructures/FiniteMap/IFiniteMap.cs
@@ -5,7 +5,7 @@
     /// </summary>
     /// <typeparam name="TKey">The key type</typeparam>
     /// <typeparam name="T">The element type</typeparam>
-    public interface FiniteMap<TKey, T>
+    public interface IFiniteMap<TKey, T>
     {
         /// <summary>
         /// Adds a new key-value binding to the dictionary.
@@ -13,7 +13,7 @@
         /// <param name="key">The key.</param>
         /// <param name="value">The value.</param>
         /// <returns>A dictionary extended with the given binding.</returns>
-        FiniteMap<TKey, T> Bind(TKey key, T value);
+        IFiniteMap<TKey, T> Bind(TKey key, T value);
 
         /// <summary>
         /// Lookup of the value for the specified key.
