53bc01a8454f6bd497e79d224efb89d5afe791e1
Jan Schwinghammer <j.schwinghammer@gmail.com>
2017-06-02 23:24:07 +0200

    Unit tests for Trie implementation of finite maps added

---
+++ src/FunctionalDataStructures.Test.UnitTests/FiniteMap/TrieTests.cs
@@ -0,0 +1,76 @@
+﻿namespace FunctionalDataStructures.Test.UnitTests.FiniteMap
+{
+    using System;
+    using FunctionalDataStructures.FiniteMap;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TrieTests : TestBase
+    {
+        [TestMethod]
+        [UnitTest]
+        public void CountOfEmptyMap()
+        {
+            var empty = Trie<int>.Empty;
+
+            Assert.AreEqual(
+                0,
+                empty.Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void CountOfSingletonMap()
+        {
+            var empty = Trie<int>.Empty;
+
+            Assert.AreEqual(
+                1,
+                empty.Bind("key1", 1).Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void CountOfNonemptyMap()
+        {
+            var empty = Trie<int>.Empty;
+
+            Assert.AreEqual(
+                2,
+                empty.Bind("key1", 1).Bind("key2", 2).Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void LookupInEmptyMapThrowsException()
+        {
+            var empty = Trie<int>.Empty;
+
+            AssertThrows<NotFoundException>(() =>
+                empty.Lookup("key"));
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void LookupOfExistingKey()
+        {
+            var empty = Trie<int>.Empty;
+            var map = empty.Bind("key1", 1).Bind("key2", 2).Bind("key3", 3);
+
+            Assert.AreEqual(
+                2,
+                map.Lookup("key2"));
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void LookupOfNonexistingKey()
+        {
+            var empty = Trie<int>.Empty;
+            var map = empty.Bind("key1", 1).Bind("key2", 2).Bind("key3", 3);
+
+            AssertThrows<NotFoundException>(() =>
+                empty.Lookup("invalidKey"));
+        }
+    }
+}
