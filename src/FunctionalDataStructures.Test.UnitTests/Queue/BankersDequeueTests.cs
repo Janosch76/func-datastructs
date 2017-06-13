3c5f2d766b9356554d936d87faf59ef2d30ae3d4
Jan Schwinghammer <j.schwinghammer@gmail.com>
2017-06-13 22:24:49 +0200

    unit tests for double-ended queues added

---
+++ src/FunctionalDataStructures.Test.UnitTests/Queue/BankersDequeueTests.cs
@@ -0,0 +1,135 @@
+﻿namespace FunctionalDataStructures.Test.UnitTests.Queue
+{
+    using System;
+    using System.Linq;
+    using FunctionalDataStructures.Queue;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BankersDequeueTests : TestBase
+    {
+        [TestMethod]
+        [UnitTest]
+        public void CountOfEmptyDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                0,
+                empty.Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void CountOfSingletonDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                1,
+                empty.Snoc(1).Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void CountOfNonemptyDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                2,
+                empty.Snoc(1).Snoc(2).Count);
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void IsEmptyOnEmptyDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                true,
+                empty.IsEmpty());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void IsEmptyOnSingletonDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                false,
+                empty.Snoc(1).IsEmpty());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void HeadOnEmptyDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            AssertThrows<EmptyCollectionException>(() =>
+                empty.Head());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void HeadOnSingletonDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                1,
+                empty.Snoc(1).Head());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void TailOnEmptyDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            AssertThrows<EmptyCollectionException>(() =>
+                empty.Tail());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void TailOnSingletonDequeue()
+        {
+            var empty = BankersDequeue<int>.Empty;
+
+            Assert.AreEqual(
+                true,
+                empty.Snoc(1).Tail().IsEmpty());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void FifoBehaviorOfDequeue()
+        {
+            var queue = BankersDequeue<int>.Empty.Snoc(1).Snoc(2);  // [1,2]
+
+            Assert.AreEqual(
+                1,
+                queue.Head());
+
+            Assert.AreEqual(
+                2,
+                queue.Tail().Head());
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void FifoEnumerationOfDequeue()
+        {
+            var queue = BankersDequeue<int>.Empty.Snoc(1).Snoc(2);  // [1,2]
+            var elements = queue.ToArray();
+
+            Assert.AreEqual(2, elements.Length);
+            Assert.AreEqual(1, elements[0]);
+            Assert.AreEqual(2, elements[1]);
+        }
+    }
+}
---
+++ src/FunctionalDataStructures/Queue/BankersDequeue.cs
@@ -0,0 +1,81 @@
+﻿namespace FunctionalDataStructures.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using FunctionalDataStructures.Utils;
+
+    /// <summary>
+    /// Amortized double-ended queue implementation using the Bankers Method, 
+    /// from Chris Okasaki´s book, p. 64ff
+    /// </summary>
+    /// <typeparam name="T">The element type</typeparam>
+    public class BankersDequeue<T> : IDequeue<T>
+    {
+        /// <summary>
+        /// The empty dequeue.
+        /// </summary>
+        public static readonly BankersDequeue<T> Empty = new BankersDequeue<T>(0, 0);
+
+        private int frontLength, rearLength;
+
+        public BankersDequeue(int frontLength, int rearLength)
+        {
+            this.frontLength = frontLength;
+            this.rearLength = rearLength;
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IQueue<T> Cons(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Head()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueue<T> Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Last()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueue<T> Snoc(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueue<T> Tail()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
