f2f05c47e39d05336d3c9e32076a5ccc7e7a0464
Jan Schwinghammer <j.schwinghammer@gmail.com>
2017-07-23 23:40:45 +0200

    initial (untested) version of Trie of Trees implementation

---
+++ src/FunctionalDataStructures/FiniteMap/TrieOfTrees.cs
@@ -0,0 +1,197 @@
+﻿namespace FunctionalDataStructures.FiniteMap
+{
+    using System;
+    using FunctionalDataStructures.Utils;
+
+    /// <summary>
+    /// Trie implementation of functional finite maps over tree-structured keys
+    /// </summary>
+    /// <typeparam name="TKey">The key element type</typeparam>
+    /// <typeparam name="T">The element type</typeparam>
+    public class TrieOfTrees<TKey, T> : IFiniteMap<BinaryTree<TKey>, T>
+        where TKey : IComparable<TKey>
+    {
+        /// <summary>
+        /// The empty trie.
+        /// </summary>
+        public static readonly TrieOfTrees<TKey, T> Empty = new TrieOfTrees<TKey, T>(Option<T>.None, AssociationList<TKey, TrieOfTrees<TKey, TrieOfTrees<TKey, T>>>.Empty);
+
+        private readonly Option<T> node;
+        private readonly AssociationList<TKey, TrieOfTrees<TKey, TrieOfTrees<TKey, T>>> edges;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrieOfTrees{TKey, T}"/> class.
+        /// </summary>
+        /// <param name="node">The node.</param>
+        /// <param name="edges">The edge map.</param>
+        private TrieOfTrees(Option<T> node, AssociationList<TKey, TrieOfTrees<TKey, TrieOfTrees<TKey, T>>> edges)
+        {
+            this.node = node;
+            this.edges = edges;
+
+            Count = (node.HasValue ? 1 : 0);
+            foreach (var edge in edges)
+            {
+                var subtrie = edge.Value;
+                Count += subtrie.Count;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of elements in the map.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Determines whether this instance is empty.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        /// <summary>
+        /// Lookup of the value for the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>
+        /// The value associated with the given key
+        /// </returns>
+        public T Lookup(BinaryTree<TKey> key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (key.IsEmpty() && this.node.HasValue)
+            {
+                return this.node.Value;
+            }
+            else if (key.IsEmpty() && !this.node.HasValue)
+            {
+                throw new NotFoundException();
+            }
+            else
+            {
+                TKey k = key.Element;
+                BinaryTree<TKey> left = key.Left;
+                BinaryTree<TKey> right = key.Right;
+                return edges.Lookup(k).Lookup(left).Lookup(right);
+            }
+        }
+
+
+        /// <summary>
+        /// Adds a new key-value binding to the dictionary.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// A dictionary extended with the given binding.
+        /// </returns>
+        IFiniteMap<BinaryTree<TKey>, T> IFiniteMap<BinaryTree<TKey>, T>.Bind(BinaryTree<TKey> key, T value)
+        {
+            return Bind(key, value);
+        }
+
+        /// <summary>
+        /// Adds a new key-value binding to the trie.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// A trie extended with the given binding.
+        /// </returns>
+        public TrieOfTrees<TKey, T> Bind(BinaryTree<TKey> key, T value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (key.IsEmpty())
+            {
+                return new TrieOfTrees<TKey, T>(Option<T>.Some(value), this.edges);
+            }
+            else
+            {
+                TKey k = key.Element;
+                BinaryTree<TKey> left = key.Left;
+                BinaryTree<TKey> right = key.Right;
+
+                var subtrie1 = TryGetSubtrie(k);
+                var subtrie2 = TryGetSubtrie(subtrie1, left);
+                var updatedSubtrie1 = subtrie2.Bind(right, value);
+                var updatedSubtrie2 = subtrie1.Bind(left, updatedSubtrie1);
+                return new TrieOfTrees<TKey, T>(this.node, this.edges.Bind(k, updatedSubtrie2));
+            }
+        }
+
+        private TrieOfTrees<TKey, T> TryGetSubtrie(TrieOfTrees<TKey, TrieOfTrees<TKey, T>> t, BinaryTree<TKey> left)
+        {
+            try
+            {
+                return t.Lookup(left);
+            }
+            catch (NotFoundException)
+            {
+                return TrieOfTrees<TKey, T>.Empty;
+            }
+        }
+
+        private TrieOfTrees<TKey, TrieOfTrees<TKey, T>> TryGetSubtrie(TKey k)
+        {
+            try
+            {
+                return this.edges.Lookup(k);
+            }
+            catch (NotFoundException)
+            {
+                return TrieOfTrees<TKey, TrieOfTrees<TKey, T>>.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
+        public System.Collections.Generic.IEnumerator<Binding<BinaryTree<TKey>, T>> GetEnumerator()
+        {
+            if (this.node.HasValue)
+            {
+                yield return new Binding<BinaryTree<TKey>, T>(BinaryTree<TKey>.Empty, this.node.Value);
+            }
+
+            foreach (var edge in this.edges)
+            {
+                TKey k = edge.Key;
+                foreach (var subtrie1 in edge.Value)
+                {
+                    foreach (var subtrie2 in subtrie1.Value)
+                    {
+                        yield return new Binding<BinaryTree<TKey>, T>(
+                            BinaryTree<TKey>.MakeTree(k, subtrie1.Key, subtrie2.Key),
+                            subtrie2.Value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/FunctionalDataStructures/Utils/BinaryTree.cs
@@ -0,0 +1,120 @@
+﻿namespace FunctionalDataStructures.Utils
+{
+    using System;
+
+    /// <summary>
+    /// Minimalist binary tree implementation.
+    /// </summary>
+    /// <typeparam name="T">The element type</typeparam>
+    public abstract class BinaryTree<T> : IComparable<BinaryTree<T>>
+    {
+        /// <summary>
+        /// The empty binary tree.
+        /// </summary>
+        public static readonly BinaryTree<T> Empty = new EmptyTree();
+
+        /// <summary>
+        /// Gets the element stored at an inner node.
+        /// </summary>
+        public abstract T Element { get; }
+
+        /// <summary>
+        /// Gets the left subtree.
+        /// </summary>
+        public abstract BinaryTree<T> Left { get; }
+
+        /// <summary>
+        /// Gets the right subtree.
+        /// </summary>
+        public abstract BinaryTree<T> Right { get; }
+
+        /// <summary>
+        /// Creates a non-empty tree.
+        /// </summary>
+        /// <param name="element">The root element.</param>
+        /// <param name="left">The left subtree.</param>
+        /// <param name="right">The right subtree.</param>
+        /// <returns></returns>
+        public static BinaryTree<T> MakeTree(T element, BinaryTree<T> left, BinaryTree<T> right)
+        {
+            return new Node(element, left, right);
+        }
+
+        /// <summary>
+        /// Compares the current object with another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the <paramref name="other" /> parameter.Zero This object is equal to <paramref name="other" />. Greater than zero This object is greater than <paramref name="other" />.
+        /// </returns>
+        public int CompareTo(BinaryTree<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether this instance is empty.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </returns>
+        public abstract bool IsEmpty();
+
+        private class EmptyTree : BinaryTree<T>
+        {
+            public override T Element
+            {
+                get { throw new EmptyCollectionException(); }
+            }
+
+            public override BinaryTree<T> Left
+            {
+                get { throw new EmptyCollectionException(); }
+            }
+
+            public override BinaryTree<T> Right
+            {
+                get { throw new EmptyCollectionException(); }
+            }
+
+            public override bool IsEmpty()
+            {
+                return true;
+            }
+        }
+
+        private class Node : BinaryTree<T>
+        {
+            private readonly T element;
+            private readonly BinaryTree<T> left;
+            private readonly BinaryTree<T> right;
+
+            public Node(T element, BinaryTree<T> left, BinaryTree<T> right)
+            {
+                this.element = element;
+                this.left = left;
+                this.right = right;
+            }
+
+            public override T Element
+            {
+                get { return this.element; }
+            }
+
+            public override BinaryTree<T> Left
+            {
+                get { return this.left; }
+            }
+
+            public override BinaryTree<T> Right
+            {
+                get { return this.right; }
+            }
+
+            public override bool IsEmpty()
+            {
+                return false;
+            }
+        }
+    }
+}
